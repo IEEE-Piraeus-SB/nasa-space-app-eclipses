220b551bc49a94ef51a60d259bec556b2f3bf76c
Irene <146589861+nirilo@users.noreply.github.com>
2023-10-01 02:03:48 +0300

    Difference between solar and lunar eclipse

---
+++ src/script.js
@@ -0,0 +1,4 @@
+const solarEclipse = document.querySelector('.eclipse:nth-child(1)');
+const lunarEclipse = document.querySelector('.eclipse:nth-child(2)');
+const eclipseContainers = document.querySelectorAll('.eclipse-container');
+
