c06fbde08a136e96187a35308f92e3bddcd6e87f
JustDankas <103999035+JustDankas@users.noreply.github.com>
2023-10-01 16:08:52 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const blackBackground = document.getElementById("black-background");
-const arrow = document.querySelector(".fa-arrow-down");
+// const arrow = document.querySelector(".fa-arrow-down");
 const textContainer = document.getElementById("text-container");
 const moonImage = document.getElementById("moonImage");
 const eclipse = document.getElementById("eclipse");
 
-arrow.addEventListener("click", () => {
-  window.scrollTo(window.scrollX, window.scrollY + 400);
-  console.log("hello");
-});
+const firstParagraph="Info text 1";
+const secondParagraph="Info text 2";
+const thirdParagraph="Info text 3";
+
+const moonShadowStart=200;
+const sunStart=600;
+// arrow.addEventListener("click", () => {
+//   window.scrollTo(window.scrollX, window.scrollY + 400);
+//   console.log("hello");
+// });
 window.addEventListener("scroll", () => {
-  if (window.scrollY >= 200 && window.scrollY < 600) {
+  if (window.scrollY >= moonShadowStart && window.scrollY < sunStart) {
     blackBackground.style.top = "20%";
     textContainer.classList.remove("hidden");
     textContainer.animate([{ opacity: "0" }, { opacity: "1" }], {
@@ -19,24 +25,27 @@ window.addEventListener("scroll", () => {
     textContainer.style.opacity = "1";
     eclipse.style.opacity = 0;
     moonImage.style.opacity = 1;
+    eclipse.style.backgroundColor = "orange";
+    textContainer.innerText = secondParagraph;
+
 
     // blackBackground.children[0].classList.remove("hidden");
     console.log(window.scrollY);
-  } else if (window.scrollY > 600 && window.scrollY < 3000) {
+  } else if (window.scrollY > sunStart && window.scrollY < 3000) {
     blackBackground.style.top = "-10%";
     textContainer.classList.remove("hidden");
     textContainer.animate([{ opacity: "0" }, { opacity: "1" }], {
       duration: 500,
       iterations: 1,
     });
-    textContainer.innerText = "Info text 2";
+    textContainer.innerText = thirdParagraph;
     textContainer.style.opacity = "1";
     moonImage.style.opacity = 0;
     eclipse.style.opacity = 1;
     eclipse.style.backgroundColor = "black";
 
     // blackBackground.style.opacity = "0";
-  } else if (window.scrollY < 200) {
+  } else if (window.scrollY < moonShadowStart) {
     blackBackground.style.top = "100%";
     // blackBackground.children[0].classList.add("hidden");
     // blackBackground.children[1].classList.add("hidden");
@@ -47,7 +56,8 @@ window.addEventListener("scroll", () => {
     });
     moonImage.style.opacity = 1;
     textContainer.style.opacity = "0";
-    textContainer.innerText = "Info text 1";
-    eclipse.style.opacity = 1;
+    textContainer.innerText = firstParagraph;
+    eclipse.style.opacity = 0;
+    eclipse.style.backgroundColor = "orange";
   }
 });
