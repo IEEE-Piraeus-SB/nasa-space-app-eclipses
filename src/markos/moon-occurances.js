dc9cb5874ea78735f107f6fa18b23cc599aa5d09
JustDankas <103999035+JustDankas@users.noreply.github.com>
2023-10-01 16:13:15 +0300

    comment

--- src/markos/moon-occurances.js
+++ src/markos/moon-occurances.js
@@ -1,21 +1,21 @@
-const counter = document.getElementById("moon-occurance-counter");
+// const counter = document.getElementById("moon-occurance-counter");
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * 9 + 1);
-}
+// function getRandomNumber() {
+//   return Math.floor(Math.random() * 9 + 1);
+// }
 
-const randArr = [];
-randArr.push(4);
+// const randArr = [];
+// randArr.push(4);
 
-for (let i = 0; i < 20; i++) {
-  randArr.push(getRandomNumber());
-}
+// for (let i = 0; i < 20; i++) {
+//   randArr.push(getRandomNumber());
+// }
 
-// setInterval()
+// // setInterval()
 
-const interval = setInterval(() => {
-  let text = randArr.pop();
-  counter.innerHTML = text;
-  console.log(text, "blabla");
-  if (randArr.length == 0) clearInterval(interval);
-}, 200);
+// const interval = setInterval(() => {
+//   let text = randArr.pop();
+//   counter.innerHTML = text;
+//   console.log(text, "blabla");
+//   if (randArr.length == 0) clearInterval(interval);
+// }, 200);
