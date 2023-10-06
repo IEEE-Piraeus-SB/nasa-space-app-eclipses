bcd5588445e3d668c333f04ce0f1439caec0310f
Irene <irini.loli@outlook.com>
2023-10-06 23:03:08 +0300

    Squashed commit of the following:
    commit 738f2c1cd126a3ccea6b70881ba2692fbfa983e1
    Merge: d0038e4 4b5ea65
    Author: IEEE-Piraeus-SB <131485967+IEEE-Piraeus-SB@users.noreply.github.com>
    Date:   Fri Oct 6 19:48:23 2023 +0300
    
        Merge pull request #3 from IEEE-Piraeus-SB/Markos
    
        some fixes with classes and ids
    
    commit 4b5ea6542165a8371aeaa8ff1b3b6af8cc5ec7d6
    Author: JustDankas <jan-haspel@hotmail.com>
    Date:   Fri Oct 6 19:47:34 2023 +0300
    
        some fixes with classes and ids

--- src/nasa-space/script_irini.js
+++ src/nasa-space/script_irini.js
@@ -1,43 +1,38 @@
-const solarEclipse = document.querySelector('.eclipse:nth-child(1)');
-const lunarEclipse = document.querySelector('.eclipse:nth-child(2)');
-const eclipseContainers = document.querySelectorAll('.eclipse-container');
+const solarEclipse = document.querySelector(".eclipse:nth-child(1)");
+const lunarEclipse = document.querySelector(".eclipse:nth-child(2)");
+const eclipseContainers = document.querySelectorAll(".eclipse-container");
 
-const moon = document.getElementById("moon-solar-lunar")
-const button= document.getElementById("change-solar-to-lunar-btn")
+const moonObject = document.getElementById("moon-solar-lunar");
+const button = document.getElementById("change-solar-to-lunar-btn");
 
-const lunarText = document.getElementById("text-lunar")
-const solarText = document.getElementById("text-solar")
+const lunarText = document.getElementById("text-lunar");
+const solarText = document.getElementById("text-solar");
 
-let isSolar // solar = 0 , lunar = 1
-let canClick = true
-function change_eclipse(e){
-    console.log("hello from space")
-    if(canClick){
-        canClick = false
-        isSolar = !isSolar
-    if(!isSolar){
+let isSolar; // solar = 0 , lunar = 1
+let canClick = true;
+function change_eclipse(e) {
+  console.log("hello from space");
+  if (canClick) {
+    canClick = false;
+    isSolar = !isSolar;
+    if (!isSolar) {
+      moonObject.classList.add("spin-to-solar");
+      moonObject.classList.remove("spin-to-lunar");
+      button.innerText = "Try Lunar Eclipse";
+      lunarText.classList.add("text-hidden");
+      solarText.classList.remove("text-hidden");
+    } else {
+      moonObject.classList.add("spin-to-lunar");
+      moonObject.classList.remove("spin-to-solar");
+      button.innerText = "Try Solar Eclipse";
 
-        moon.classList.add("spin-to-solar")
-        moon.classList.remove("spin-to-lunar")
-        button.innerText = "Try Lunar Eclipse";
-        lunarText.classList.add("text-hidden")
-        solarText.classList.remove("text-hidden")
-        
-    }
-    else{
-        moon.classList.add("spin-to-lunar")
-        moon.classList.remove("spin-to-solar")
-        button.innerText = "Try Solar Eclipse";
-
-        
-        solarText.classList.add("text-hidden")
-        lunarText.classList.remove("text-hidden")
-
+      solarText.classList.add("text-hidden");
+      lunarText.classList.remove("text-hidden");
     }
     setTimeout(() => {
-        canClick = true
+      canClick = true;
     }, 5000);
-    }
+  }
 }
 
-button.addEventListener("click",change_eclipse)
+button.addEventListener("click", change_eclipse);
