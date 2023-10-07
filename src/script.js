96482e4a0e8f5b45e14f199ace0b0abecbab451e
Irene <146589861+nirilo@users.noreply.github.com>
2023-10-07 03:32:29 +0300

    el Fixes (#4)
    * Squashed commit of the following:
    
    commit 738f2c1cd126a3ccea6b70881ba2692fbfa983e1
    Merge: d0038e4 4b5ea65
    Author: IEEE-Piraeus-SB <131485967+IEEE-Piraeus-SB@users.noreply.github.com>
    Date:   Fri Oct 6 19:48:23 2023 +0300
    
        Merge pull request #3 from IEEE-Piraeus-SB/Markos
    
        some fixes with classes and ids
    
    commit 4b5ea6542165a8371aeaa8ff1b3b6af8cc5ec7d6
    Author: JustDankas <jan-haspel@hotmail.com>
    Date:   Fri Oct 6 19:47:34 2023 +0300
    
        some fixes with classes and ids
    
    * Eclipse Section Still Pending
    
    Small fixes, file locations changed

--- src/script.js
+++
@@ -1,4 +0,0 @@
-const solarEclipse = document.querySelector('.eclipse:nth-child(1)');
-const lunarEclipse = document.querySelector('.eclipse:nth-child(2)');
-const eclipseContainers = document.querySelectorAll('.eclipse-container');
-
